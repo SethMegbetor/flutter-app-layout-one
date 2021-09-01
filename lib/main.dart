b5c44ba6eafe6976387ba51804fd11de9560d882
SethMegbetor <sethmegbetor@gmail.com>
2021-09-01 23:13:17 +0000

    Wednesday

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('Welcome to My App'),
         ),
         body: Column(
           children: [
@@ -52,6 +52,27 @@ class MyApp extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
