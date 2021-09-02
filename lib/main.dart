8a63b98d10583b34be60f88626c5fdab4206bcb7
SethMegbetor <sethmegbetor@gmail.com>
2021-09-02 23:55:07 +0000

    close of thursday

--- lib/main.dart
+++ lib/main.dart
@@ -70,8 +70,14 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Welcome to My App'),
         ),
-        body: Column(
+        body: ListView(
           children: [
+            Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
             titleSection,
             buttonSection,
             textSection,
